37edda51ea6ea112c687d2c59e97c3e2adb40f7e
tota79 <balashyabni@gmail.com>
2025-07-10 10:04:46 +0200

    filter out Oxee.. native

--- src/utils/filter-tokens.ts
+++ src/utils/filter-tokens.ts
@@ -2,14 +2,25 @@ import { Token } from "../types/token";
 
 const EXCLUDED_EXACT = ["WTF"];
 const EXCLUDED_PATTERNS = ["REALTOKEN"];
+const EXCLUDED_ADDRESSES = [
+  "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // 1inch native token representation
+].map((addr) => addr.toLowerCase());
 
 export function filterTokens(tokens: Token[]): Token[] {
-  return tokens.filter(({ symbol }) => {
-    const isExactExcluded = EXCLUDED_EXACT.includes(symbol);
-    const matchesPattern = EXCLUDED_PATTERNS.some((pattern) =>
-      symbol.includes(pattern)
-    );
-
-    return !isExactExcluded && !matchesPattern;
-  });
+  return tokens
+    .filter(
+      ({ symbol }) =>
+        !EXCLUDED_EXACT.includes(symbol) &&
+        !EXCLUDED_PATTERNS.some((pattern) => symbol.includes(pattern))
+    )
+    .map(
+      (token) =>
+        ({
+          ...token,
+          chains: token.chains.filter(
+            (chain) => !EXCLUDED_ADDRESSES.includes(chain.address.toLowerCase())
+          ),
+        } as Token)
+    )
+    .filter((token) => token.chains.length > 0);
 }
